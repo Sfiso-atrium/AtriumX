8db03985522594520b293231bde1e7b240613d1b
Sfiso-atrium <sfisoholdings0@gmail.com>
2026-06-14 20:50:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,20 @@
+// src/App.tsx
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { AppProvider } from './context/AppContext'
+import Entrance from './pages/Entrance'
+import Feed from './pages/Feed'
+import RetailerLanding from './pages/RetailerLanding'
 
+export default function App() {
+  return (
+    <AppProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Entrance />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/retailer" element={<RetailerLanding />} />
+        </Routes>
+      </HashRouter>
+    </AppProvider>
+  )
+}
