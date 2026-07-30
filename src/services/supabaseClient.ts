601abafb88f1251b03cdd16db91daa6cbf00312c
Sfiso-atrium <sfisoholdings0@gmail.com>
2026-07-30 06:15:57 +0200

    Delete src directory

--- src/services/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://oljpgpgeqfwrqposrups.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sanBncGdlcWZ3cnFwb3NydXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NTgwOTEsImV4cCI6MjA5NzAzNDA5MX0.Caq0ix2YcGWeox8x0ZztNQczw1UolXSbx7xoZIlsm4c'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
