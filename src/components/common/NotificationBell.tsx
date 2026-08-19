9ee058359f76bf591aa3a7f9f23a86b6fdb51e22
Sfiso-atrium <sfisoholdings0@gmail.com>
2026-08-19 06:49:46 +0200

    Update NotificationBell.tsx

--- src/components/common/NotificationBell.tsx
+++ src/components/common/NotificationBell.tsx
@@ -97,7 +97,9 @@ export default function NotificationBell() {
       navigate(`/chat/${notif.conversation_id}`)
     } else if ((notif.type === 'review' || notif.type === 'review_locked' || notif.type === 'business_approved' || notif.type === 'business_rejected') && currentUser) {
       navigate(`/profile/${currentUser.id}`)
-    } else if (notif.type === 'deadline_reminder' || notif.type === 'watchlist_match') {
+    } else if (notif.type === 'watchlist_match' && notif.listing_id) {
+      navigate(`/listing/${notif.listing_id}`)
+    } else if (notif.type === 'deadline_reminder') {
       navigate('/space')
     }
   }
