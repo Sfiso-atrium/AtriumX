e8a2abe3ebd9a4093355b71e3c653dc4f26bb850
Sfiso-atrium <sfisoholdings0@gmail.com>
2026-08-20 22:28:24 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+// autoUpdate means this silently swaps in a new version in the background
+// on the next visit - no "reload to update" prompt to build a UI for.
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
