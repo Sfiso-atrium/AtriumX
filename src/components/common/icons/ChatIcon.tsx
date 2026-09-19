00f3001677380d81c2477e9a3b79b323c2a54248
Sfiso-atrium <sfisoholdings0@gmail.com>
2026-09-19 10:25:04 +0200

    Update ChatIcon.tsx

--- src/components/common/icons/ChatIcon.tsx
+++ src/components/common/icons/ChatIcon.tsx
@@ -5,12 +5,21 @@ type IconProps = {
 
 export default function ChatIcon({ size = 24, className }: IconProps) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 16 16" className={className}>
-      <path d="M0 0h16v16H0z" fill="none" />
-      <path
-        fill="currentColor"
-        d="M8 1a7 7 0 1 1-3.538 13.04l-2.804.935a.5.5 0 0 1-.633-.633l.934-2.806A7 7 0 0 1 8 1M5.5 9a.5.5 0 0 0 0 1h3a.5.5 0 1 0 0-1zm0-3a.5.5 0 0 0 0 1h5a.5.5 0 1 0 0-1z"
-      />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.8"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      aria-hidden="true"
+    >
+      <path d="M20.5 11.5a8.5 8.5 0 0 1-8.5 8.5 8.35 8.35 0 0 1-3.5-.76L4 20.5l1.26-3.46A8.46 8.46 0 0 1 3.5 11.5a8.5 8.5 0 1 1 17 0Z" />
+      <path d="M8 10h8M8 14h5" />
     </svg>
   )
 }
